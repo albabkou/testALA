091dbe54650d8def9e2ac90f6a48825f8401cec0
lisa louison <lisa.louison@gmail.com>
2017-05-22 12:18:43 -0400

    imported booklist to app.js

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BookList from './components/booklist';
 
 class App extends Component {
   render() {
